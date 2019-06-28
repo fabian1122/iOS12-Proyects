94598f5167eee4399f9a2344a508053c354647e0
Fabian Largo <fabianlargo1@hotmail.com>
2019-06-27 23:17:23 -0500

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by Fabian Largo on 6/27/19.
+//  Copyright © 2019 DevSystem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
